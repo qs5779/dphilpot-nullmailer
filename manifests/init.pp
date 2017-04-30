8bc4817b9eabb100804f0a0cd4d1fae787aa0352
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-04-30 14:58:36 +0200

    Use puppet 4 types

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,13 @@
 # Copyright Dennis Philpot
 #
 class nullmailer (
-  $adminaddr      = undef,
-  $defaultdomain  = undef,
-  $remotes        = undef,
-  $me             = $::hostname,
-  $package_ensure = undef,
-  $package_name   = 'nullmailer',
+  Pattern['[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]'] $adminaddr,
+  String $defaultdomain,
+  Array[String] $remotes,
+  String $me = $::hostname,
+  Optional[String] $package_ensure = undef,
+  String $package_name  = 'nullmailer',
 ) {
-  validate_re($adminaddr, '[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]', 'Invalid E-Mail address')
-  validate_string($defaultdomain)
-  validate_array($remotes)
-  validate_string($me)
-
   include ::nullmailer::install
   include ::nullmailer::config
   include ::nullmailer::service
